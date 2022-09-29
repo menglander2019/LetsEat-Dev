5d292ef441a29654c039e6135a6afb4da8c9f710
Max Englander <maxenglander@Maxs-MacBook-Air.local>
2022-09-29 15:17:34 -0400

    added additional endpoints

--- app.py
+++ app.py
@@ -3,5 +3,30 @@ from fastapi import FastAPI
 app = FastAPI()
  
 @app.get("/")
-def root ():
-  return {"message": "Hello World!"}
+def home():
+    return {"message": "Hello World!"}
+
+@app.get("/about")
+def about():
+    return {"message": "about"}
+
+@app.get("/createprofile")
+def createprofile():
+    return {"message": "createprofile"}
+
+@app.get("/login")
+def login():
+    return {"message": "login page"}
+
+@app.get("/signup")
+def signup():
+    return {"message": "signup page"}
+
+@app.get("/questionnaire")
+def questionnaire():
+    return {"message": "questionnaire"}
+
+@app.get("/recommendation")
+def recommendation():
+    return {"message": "recommendation"}
+
