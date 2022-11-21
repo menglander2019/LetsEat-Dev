d7d497ab67ed4b8a3dfce0517e256e3c25a67b89
dkim114 <62488974+dkim114@users.noreply.github.com>
2022-11-21 16:58:12 -0500

    Final pushes

--- react/letseat-app/src/components/HomeComponents/HomeBanner.jsx
+++ react/letseat-app/src/components/HomeComponents/HomeBanner.jsx
@@ -11,8 +11,8 @@ const HomeBanner = () => {
                     <div className="home-main-block">
                         <div className="d-flex flex-column">
                             <p className="display-4 text-center charter-regular">Discover your new favorite restaurant nearby.</p>
-                            <div className="d-flex justify-content-center">
-                                <p className="text-center colfax-regular flex-styling-75">
+                            <div className="d-flex justify-content-center mt-3">
+                                <p className="text-center font-size-md colfax-regular flex-styling-75">
                                     Our machine learning powered algorithm will help you find new restaurants that match your unique preferences. 
                                 </p>
                             </div>
