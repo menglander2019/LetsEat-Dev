09c6b659139fc676670379f529845afe73c95f64
dkim114 <62488974+dkim114@users.noreply.github.com>
2023-04-04 06:53:59 -0400

    UI Change

--- react/letseat-app/src/pages/GroupSubmission.js
+++ react/letseat-app/src/pages/GroupSubmission.js
@@ -15,22 +15,29 @@ function GroupSubmission() {
 
     const navigate = useNavigate()
 
-    const backButton = () => {
-        navigate("/dashboard")
-    }
-
     return (
-        <div className="container-fluid group-profile-component">
-                <div className="d-flex flex-column">
-                    <div className="d-flex align-items-center justify-content-center">
-                        <div className="col-md-6 mt-5">
-                            <div className="profile-main-block">
-                                <h1 className="text-center move-medium black-theme">Group Submission Successful!</h1>
+        <div className="container-fluid dashboard-component">
+            <div className="d-flex flex-column">
+                <div className="dashboard-banner">
+                    <div className="d-flex">
+                        <div className="col-md-6">
+                            <div className="d-flex justify-content-center">
+                                <div className="col-md-8 padding-style-7">
+                                    <div className="d-flex flex-column">
+                                    <h1 className="move-medium black-theme">Group Submission Successful!</h1>
+                                    <p className="move-medium black-theme mt-3">Thank you for your submission. Your preferences will be added to the group search</p>
+                                    <p className="move-medium grey-theme mt-3">You may now close this page.</p>
+                                    </div>
+                                </div>
                             </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="group-image"></div>
                         </div>
                     </div>
                 </div>
-            </div> 
+            </div>
+        </div>
     );
 }
   
